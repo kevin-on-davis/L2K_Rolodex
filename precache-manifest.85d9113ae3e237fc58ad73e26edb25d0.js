42bf03df32755f2eb188652d166392e41c8cda98
kevin-on-davis <kevin.on.davis@gmail.com>
2019-12-09 11:26:34 -0500

    Updates

---
+++ precache-manifest.85d9113ae3e237fc58ad73e26edb25d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d2b8fea828ede17bad416e36c9c1fa",
+    "url": "/L2K_Rolodex/index.html"
+  },
+  {
+    "revision": "8099dcbbe58d4a99a6b7",
+    "url": "/L2K_Rolodex/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "123fd4755308cdb72ab5",
+    "url": "/L2K_Rolodex/static/css/main.4b0d7fcb.chunk.css"
+  },
+  {
+    "revision": "8099dcbbe58d4a99a6b7",
+    "url": "/L2K_Rolodex/static/js/2.d1c07690.chunk.js"
+  },
+  {
+    "revision": "948b2577ebde43e7361ca37ca65c844b",
+    "url": "/L2K_Rolodex/static/js/2.d1c07690.chunk.js.LICENSE"
+  },
+  {
+    "revision": "123fd4755308cdb72ab5",
+    "url": "/L2K_Rolodex/static/js/main.8b1337dd.chunk.js"
+  },
+  {
+    "revision": "4e8f9ffa07da061ba6c4",
+    "url": "/L2K_Rolodex/static/js/runtime-main.8b51e198.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/L2K_Rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
